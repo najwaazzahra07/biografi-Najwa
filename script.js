de7b84b76ac67465dbaeb1203a3c833b59eaa2d5
najwaazzahra07 <najwaazzahra0709@gmail.com>
2026-02-04 14:17:33 +0700

    Update script.js
    Signed-off-by: najwaazzahra07 <najwaazzahra0709@gmail.com>

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function toggleSection(id) {
   const section = document.getElementById(id);
+  const menuItems = document.querySelectorAll('.menu-item');
 
-  document.querySelectorAll('.content').forEach(el => {
-    if (el.id !== id) {
-      el.classList.remove('show');
+  menuItems.forEach(item => {
+    const content = item.nextElementSibling;
+
+    if (content.id !== id) {
+      content.classList.remove('show');
+      item.classList.remove('active');
     }
   });
 
   section.classList.toggle('show');
+
+  const activeButton = section.previousElementSibling;
+  activeButton.classList.toggle('active');
 }
