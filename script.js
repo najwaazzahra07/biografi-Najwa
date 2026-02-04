4d7fb3c47c54b5e013c3ab82818945a6b14cf2d6
najwaazzahra07 <najwaazzahra0709@gmail.com>
2026-02-04 13:39:05 +0700

    Update script.js
    Signed-off-by: najwaazzahra07 <najwaazzahra0709@gmail.com>

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-<div class="menu-item" onclick="toggleSection('profil')">
-  Profil
-</div>
-<div id="profil" class="content">
-  <p>Saya mahasiswa Teknik Komputer yang tertarik pada web development...</p>
-</div>
+function toggleSection(id) {
+  const section = document.getElementById(id);
 
-<div class="menu-item" onclick="toggleSection('pendidikan')">
-  Pendidikan
-</div>
-<div id="pendidikan" class="content">
-  <p>Universitas Andalas - Teknik Komputer</p>
-</div>
+  document.querySelectorAll('.content').forEach(el => {
+    if (el.id !== id) {
+      el.classList.remove('show');
+    }
+  });
 
-<div class="menu-item" onclick="toggleSection('hobi')">
-  Hobi
-</div>
-<div id="hobi" class="content">
-  <p>Desain, ngoding, dan nonton film</p>
-</div>
-
-<div class="menu-item" onclick="toggleSection('citacita')">
-  Cita-cita
-</div>
-<div id="citacita" class="content">
-  <p>Menjadi software engineer sukses 💻✨</p>
-</div>
+  section.classList.toggle('show');
+}
